fd8abac838b633db9408bdb10441e0db1145a222
Nurudeen Ajani <nurudeenolamide010@gmail.com>
2023-05-09 01:01:25 -0700

    Updated

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -5,13 +5,13 @@ from django.utils.datastructures import MultiValueDictKeyError
 
 def index(request):
     if request.method == 'POST':
-
-        city = request.POST['city'].is_valid()
-        try:
-            source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' 
+        city = request.POST['city']
+        if city.is_valid():
+            try:
+                source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' 
                                             +city+'&units=metric&appid=c37b358f857bb5917b5565975f186470').read()
-            our_data = json.loads(source)
-            data = {
+                our_data = json.loads(source)
+                data = {
                 "country_code": str(our_data['sys']['country']),
                 "coordinate": str(our_data['coord']['lon']) + ', '
                 + str(our_data['coord']['lat']),
@@ -22,12 +22,11 @@ def index(request):
                 'main': str(our_data['weather'][0]['main']),
                 'description': str(our_data['weather'][0]['description']),
                 'icon': our_data['weather'][0]['icon'],
-            }
+                }
 
-            print (data)
-        except:
-            print ('Enter the correct city name')
-
+                print (data)
+            except:
+                print("An error occurred")
     else:
         data = {}
     return render(request, 'index.html', data)
