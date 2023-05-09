de1863811f95e39c498e01169d403eefaad514b9
Nurudeen Ajani <nurudeenolamide010@gmail.com>
2023-05-09 00:41:57 -0700

    Updated

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -5,24 +5,29 @@ from django.utils.datastructures import MultiValueDictKeyError
 
 def index(request):
     if request.method == 'POST':
-        city = request.POST['city']
-        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' 
-                                        +city+'&units=metric&appid=c37b358f857bb5917b5565975f186470').read()
-        our_data = json.loads(source)
-        data = {
-            "country_code": str(our_data['sys']['country']),
-            "coordinate": str(our_data['coord']['lon']) + ', '
-            + str(our_data['coord']['lat']),
 
-            "temp": str(our_data['main']['temp']) + ' °C',
-            "pressure": str(our_data['main']['pressure']),
-            "humidity": str(our_data['main']['humidity']),
-            'main': str(our_data['weather'][0]['main']),
-            'description': str(our_data['weather'][0]['description']),
-            'icon': our_data['weather'][0]['icon'],
-        }
+        city = request.POST['city'].is_valid()
+        try:
+            source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' 
+                                            +city+'&units=metric&appid=c37b358f857bb5917b5565975f186470').read()
+            our_data = json.loads(source)
+            data = {
+                "country_code": str(our_data['sys']['country']),
+                "coordinate": str(our_data['coord']['lon']) + ', '
+                + str(our_data['coord']['lat']),
 
-        print (data)
+                "temp": str(our_data['main']['temp']) + ' °C',
+                "pressure": str(our_data['main']['pressure']),
+                "humidity": str(our_data['main']['humidity']),
+                'main': str(our_data['weather'][0]['main']),
+                'description': str(our_data['weather'][0]['description']),
+                'icon': our_data['weather'][0]['icon'],
+            }
+
+            print (data)
+        except:
+            print ('Enter the correct city name')
+
     else:
         data = {}
     return render(request, 'index.html', data)
